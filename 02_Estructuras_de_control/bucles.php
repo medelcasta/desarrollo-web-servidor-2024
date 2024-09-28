c68fcc3011dcfea522bd72a10385b2e3bce0041a
Aurora Medel Ruiz <medelcasta@MacBook-Air-de-Aurora.local>
2024-09-28 17:38:50 +0200

    ejercicios casa

--- 02_Estructuras_de_control/bucles.php
+++ 02_Estructuras_de_control/bucles.php
@@ -27,7 +27,46 @@
         echo"</ul>";
     ?>
     <!--EJERCICIO 1: MOSTRAR EN UNA LISTA LOS NUMEROS MULTIPLOS DE 3 USANDO WHILE E IF-->
+    <?php
+    $num = 0; 
+    while ($num < 50){
+        if($num % 3 == 0){
+            echo "$num es multiplo de 3";
+        }
+        $num++;
+    }
+    ?>
     <!--EJERCICIO 2: CALCULAR LA SUMA DE LOS NUMEROS PARES ENTRE 1 Y 20-->
+    <?php
+    $num = 0; 
+    $suma = 0;
+    while ($num < 20){
+        if($num % 2 == 0){
+            $suma += $num;
+        }
+        $num++;
+    }
+    echo "La suma de los 20 primeros numeros pares es $suma";
+    ?>
     <!--EJERCICIO 3: CALCULAR EL FACTORIAL DE 6 CON WHILE-->
+    <?php
+    $num= 5; 
+    $i = 1;
+    $factorial = 1;
+    if($num < 0){
+        $factorial = " ";
+        echo "ERROR. No se puede calcular el factorial de un numero negativo";
+    }
+    else{
+        while ($i <= $num && $num >= 0){
+            $factorial = $factorial * $i;
+            $i++;
+            if($num == 0){
+                $factorial = 1;
+            }
+        }
+        echo "El factorial de $num es $factorial";
+    }
+    ?>
 </body>
 </html>
