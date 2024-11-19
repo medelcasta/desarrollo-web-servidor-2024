e2249cec7f0d4faa9dd2dfbe629f3565216458a5
aurora <auroramedel03@gmail.com>
2024-11-19 20:53:43 +0100

    ejercicios

--- 06_bases_de_datos/consolas/nuevo_estudio.php
+++ 06_bases_de_datos/consolas/nuevo_estudio.php
@@ -10,34 +10,35 @@
         }
     </style>
     <?php
-     error_reporting( E_ALL );
-     ini_set("display_errors", 1 );  
-     require 'conexion.php'; ?>
+        error_reporting( E_ALL );
+        ini_set("display_errors", 1 );  
+        require('conexion.php');
+    ?>
 </head>
 <body>
     <?php 
-    if($_SERVER["REQUEST_METHOD"]== "POST"){
-        $nombre_estudio = $_POST["nombre_estudio"];
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $nombre = $_POST["nombre"];
         $fabricante= $_POST["fabricante"];
         $generacion = $_POST["generacion"];
         $unidades_vendidas = $_POST["unidades_vendidas"];
 
-        $sql = "INSERT INTO consolas (nombre_estudio, fabricante, generacion, unidades_vendidas)
-                VALUES ('$nombre_estudio', '$fabricante', $generacion, '$unidades_vendidas')";
+        $sql = "INSERT INTO consolas (nombre, fabricante, generacion, unidades_vendidas)
+                VALUES ('$nombre', '$fabricante', $generacion, '$unidades_vendidas')";
 
-            $_conexion -> query($sql);
+        $_conexion -> query($sql);
     }
     ?>
     <form action="" method="post">
         <h1>Formulario Nuevo Estudio</h1>
         <label>Nombre Estudio</label>
-        <input type="text" name="nombre_estudio">
+        <input type="text" name="nombre">
         <label>Fabricante</label>
         <input type="text" name="fabricante">
         <label>Generacion</label>
         <input type="text" name="generacion">
         <label>Unidades Vendidas</label>
-        <input type="text" name="unidades_ventidas">
+        <input type="text" name="unidades_vendidas">
 
         <input type="submit" value="Enviar">
     </form>
